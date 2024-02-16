2ddcd25a79ab418718089f419dc44e003e42423f
Thibault Dethier <tdethier@bluesquarehub.com>
2024-02-16 18:00:04 +0100

    Added multiple Products management
    Now, there are 4 configured products: one for minors, one for the elderly, one for indigents and one for pregnant women.
    The autoenrollment process will check whether the received insuree belongs to one of the categories and enroll them in the correct product.

--- autoenroll/apps.py
+++ autoenroll/apps.py
@@ -3,7 +3,10 @@ from django.apps import AppConfig
 MODULE_NAME = "autoenroll"
 
 DEFAULT_CFG = {
-    "autoenroll_product_code": "DENTAL",
+    "autoenroll_product_minors": "NOT_CONFIGURED",
+    "autoenroll_product_elderly": "NOT_CONFIGURED",
+    "autoenroll_product_pregnant_women": "NOT_CONFIGURED",
+    "autoenroll_product_indigents": "NOT_CONFIGURED",
 }
 
 
@@ -11,11 +14,16 @@ class AutoenrollConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = MODULE_NAME
 
-    autoenroll_product_code = None
-
+    autoenroll_product_minors = None
+    autoenroll_product_elderly = None
+    autoenroll_product_pregnant_women = None
+    autoenroll_product_indigents = None
 
     def _configure_autoenroll(self, cfg):
-        AutoenrollConfig.autoenroll_product_code = cfg["autoenroll_product_code"]
+        AutoenrollConfig.autoenroll_product_minors = cfg["autoenroll_product_minors"]
+        AutoenrollConfig.autoenroll_product_elderly = cfg["autoenroll_product_elderly"]
+        AutoenrollConfig.autoenroll_product_pregnant_women = cfg["autoenroll_product_pregnant_women"]
+        AutoenrollConfig.autoenroll_product_indigents = cfg["autoenroll_product_indigents"]
 
     def ready(self):
         from core.models import ModuleConfiguration
--- autoenroll/services.py
+++ autoenroll/services.py
@@ -16,32 +16,42 @@ def autoenroll_family(insuree, family=None):
         return
     if not family:
         family = insuree.family
-    if is_family_eligible_for_autoenroll(insuree):
-        product = get_autoenroll_product()
+    product_code = determine_eligibility_to_autoenrollment(insuree, family)
+    if product_code:
+        product = get_autoenroll_product(product_code)
         policy, policy_created = get_or_create_policy(insuree, family, product)
         if policy_created:
-            logger.info(f"Autoenrolled insuree {insuree.id} into policy {policy.id}")
+            logger.info(f"Autoenrolled insuree {insuree.id} into policy {policy.id} - product {product_code}")
         else:
-            logger.info(f"Insuree {insuree.id} already autoenrolled into policy {policy.id}")
+            logger.info(f"Insuree {insuree.id} already autoenrolled into policy {policy.id} - product {product_code}")
 
 
-def is_family_eligible_for_autoenroll(insuree):
-    """
-    For this version, we'll manually check the age of the insuree (<5 or >=65)
-    """
-    if insuree.age() < 5 or insuree.age() >= 65:
-        return True
-    return False
+def determine_eligibility_to_autoenrollment(insuree, family):
+    # Checks whether or not there needs to be an autoenrollment
+
+    if insuree.age() < 5:
+        return AutoenrollConfig.autoenroll_product_minors
+    elif insuree.age() >= 65:
+        return AutoenrollConfig.autoenroll_product_elderly
+
+    # hasattr so that there is no crash for insuree.is_pregnant if this custom dev doesn't exist on the instance
+    if hasattr(insuree, "is_pregnant") and insuree.is_pregnant:
+        return AutoenrollConfig.autoenroll_product_pregnant_women
+
+    if family.poverty:
+        return AutoenrollConfig.autoenroll_product_indigents
+
+    return None
 
 
-def get_autoenroll_product():
+def get_autoenroll_product(code):
     """
     Autoenroll needs to know which product to create, this gets product configured into the app.
     """
     try:
-        return Product.objects.get(code=AutoenrollConfig.autoenroll_product_code, validity_to__isnull=True)
+        return Product.objects.get(code=code, validity_to__isnull=True)
     except Product.DoesNotExist:
-        logger.error(f"Autoenroll product {AutoenrollConfig.autoenroll_product_code} not found")
+        logger.error(f"Autoenroll product {code} not found")
         raise
 
 
